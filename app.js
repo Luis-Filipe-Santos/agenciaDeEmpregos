6750e5e88bb1cc422b991a3ef6ca18f1844886f4
Luis-Filipe-Santos <109242677+Luis-Filipe-Santos@users.noreply.github.com>
2022-10-17 16:49:14 -0300

    f

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const db = require("./db/connection");
+
 const PORT = 3000;
 
-app.listen(PORT, function() {
-    console.log(`O Express está rodando na porta ${PORT}`);
+app.listen(PORT, function () {
+  console.log(`O Express está rodando na porta ${PORT}`);
+});
+
+// db connection
+db.authenticate()
+  .then(() => {
+    console.log("Conectou ao banco com sucesso!");
+  })
+  .catch((err) => {
+    console.log("Ocorreu um erro ao conectar", err);
+  });
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Está funcionando 2");
 });
